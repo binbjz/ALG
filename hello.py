0b30e5d4be0c04ca162764931809fd7348d14b76
Bin Zhao <zhaobin.gm@gmail.com>
2015-01-15 20:43:32 +0800

    Fixed hello.py according to master branch

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ print 'Hello world!'
 print 'I add the modified file'
 print 'I am another people'
 print 'This is another test..'
-print 'This is 2015.. && Modify dev branch..
+print 'This is 2015.. && Modify dev branch..'
