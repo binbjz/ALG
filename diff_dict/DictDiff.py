ce72dcb69319b49e2ec668e6fa369fcb9cdfa322
Bin Zhao <zhaobin.gm@gmail.com>
2016-04-11 19:05:11 +0800

    update diff dict

--- diff_dict/DictDiff.py
+++ diff_dict/DictDiff.py
@@ -29,16 +29,23 @@ class dict_cls(object):
         return diff
 
 if __name__ == '__main__':
+    # Keys and values are the same
     dict1 = {1:'student', 2:'teacher', 3:'friend'}
     dict2 = {1:'student', 2:'teacher', 3:'friend'}
 
+    # Keys are the same, but the values are different
     dict3 = {11:'student', 12:'teacher', 13:'friend'}
     dict4 = {11:'students', 12:'teachers', 13:'friends'}
 
+    # Values are the same, but the Keys are different
     dict5 = {21:'student', 22:'teacher', 23:'friend'}
     dict6 = {31:'student', 32:'teacher', 33:'friend'}
 
-    print dict_cls.dict_diff(dict1, dict2)
-    print dict_cls.dict_diff(dict4, dict3)
-    print dict_cls.dict_diff(dict5, dict6)
-    print dict_cls.dict_diff(dict5, dict3)
+    print 'Keys and values are the same: \n%s' \
+            % dict_cls.dict_diff(dict1, dict2)
+    print '\nKeys are the same, but the values are different: \n%s' \
+            % dict_cls.dict_diff(dict4, dict3)
+    print '\nValues are the same, but the Keys are different: \n%s' \
+            % dict_cls.dict_diff(dict5, dict6)
+    print '\nValues and Keys are different: \n%s' \
+            % dict_cls.dict_diff(dict3, dict6)
